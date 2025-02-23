b0406deafd5120596cb8cc494c974ba765877f36
vicbvs <vicbvs@github.com>
2025-02-23 17:43:52 +0100

    Use existing components instead of non-existent Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
-import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Contracts from './components/Contracts';
 import { AppProvider } from './context/AppContext';
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
       <CssBaseline />
       <AppProvider>
         <Router>
+          <Navbar />
           <Routes>
-            {/* Toutes les routes mènent à Home */}
-            <Route path="*" element={<Home />} />
+            {/* Toutes les routes mènent à Contracts */}
+            <Route path="*" element={<Contracts />} />
           </Routes>
         </Router>
       </AppProvider>
